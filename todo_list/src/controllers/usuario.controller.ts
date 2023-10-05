7f82404e6b5473d970de417b50fb6ce9c32e67ba
LamequeFernandes <lamequesao@gmail.com>
2023-10-05 20:00:30 -0300

    correcao em identacoes

--- todo_list/src/controllers/usuario.controller.ts
+++ todo_list/src/controllers/usuario.controller.ts
@@ -1,58 +1,62 @@
 import { Response, Request } from "express";
-import { alterUsuario, createUsuario, getAllUsuarios, removeUsuario } from "../repository/usuario.repo";
+import {
+  alterUsuario,
+  createUsuario,
+  getAllUsuarios,
+  removeUsuario,
+} from "../repository/usuario.repo";
 import { UsuarioPostSchema, UsuarioPutSchema } from "../schemas/usuario.schema";
 
 export async function getUsuarios(req: Request, res: Response) {
-    try {
-        const usuarios = await getAllUsuarios();
-        res.status(200).json(usuarios);
-    } catch (e: any) {
-        console.log(e)
-        res.status(400).json(e);
-    }
+  try {
+    const usuarios = await getAllUsuarios();
+    res.status(200).json(usuarios);
+  } catch (e: any) {
+    console.log(e);
+    res.status(400).json(e);
+  }
 }
 
 export async function postUsuario(req: Request, res: Response) {
-    try {
-        const body = UsuarioPostSchema.safeParse(req.body);
-    
-        if (!body.success) {
-            res.status(422).send(body.error);
-            return;
-        }
-    
-        const result = await createUsuario(body.data);
-        res.status(201).json(result);
-    } catch(error: any) {
-        res.status(400).json(JSON.parse(error));
+  try {
+    const body = UsuarioPostSchema.safeParse(req.body);
+
+    if (!body.success) {
+      res.status(422).send(body.error);
+      return;
     }
+
+    const result = await createUsuario(body.data);
+    res.status(201).json(result);
+  } catch (error: any) {
+    res.status(400).json(JSON.parse(error));
+  }
 }
 
 export async function putUsuario(req: Request, res: Response) {
-    try {
-        const idUsuario = req.params.id;
-        const camposAlterados = UsuarioPutSchema.safeParse(req.body);
+  try {
+    const idUsuario = req.params.id;
+    const camposAlterados = UsuarioPutSchema.safeParse(req.body);
 
-        if (!camposAlterados.success) {
-            res.status(422).send(camposAlterados.error);
-            return;
-        }
-
-        const result = await alterUsuario(Number(idUsuario), camposAlterados.data);
-        res.status(200).json(result);
-
-    } catch(error: any) {
-        res.status(400).json(JSON.parse(error));
+    if (!camposAlterados.success) {
+      res.status(422).send(camposAlterados.error);
+      return;
     }
+
+    const result = await alterUsuario(Number(idUsuario), camposAlterados.data);
+    res.status(200).json(result);
+  } catch (error: any) {
+    res.status(400).json(JSON.parse(error));
+  }
 }
 
 export async function deleteUsuario(req: Request, res: Response) {
-    try {
-        const idUsuario = req.params.id;
+  try {
+    const idUsuario = req.params.id;
 
-        const result = await removeUsuario(Number(idUsuario));
-        res.status(200).json(result);
-    } catch(error: any) {
-        res.status(400).json(JSON.parse(error));
-    }
+    const result = await removeUsuario(Number(idUsuario));
+    res.status(200).json(result);
+  } catch (error: any) {
+    res.status(400).json(JSON.parse(error));
+  }
 }
--- todo_list/src/repository/usuario.repo.ts
+++ todo_list/src/repository/usuario.repo.ts
@@ -3,89 +3,88 @@ import { prisma } from "../services/prisma";
 import { hash } from "bcryptjs";
 
 export async function getAllUsuarios() {
-    try {
-        const usuarios = await prisma.usuario.findMany();
-        return usuarios;
-    } catch(error: any) {
-        throw error;
-    }
+  try {
+    const usuarios = await prisma.usuario.findMany();
+    return usuarios;
+  } catch (error: any) {
+    throw error;
+  }
 }
 
 export async function getUsuarioById(id: number) {
-    try {
-        const usuario = await prisma.usuario.findUnique({
-            where: {
-                id_usuario: id
-            }
-        });
-        return usuario;
-    } catch(error: any) {
-        throw error;
-    }
+  try {
+    const usuario = await prisma.usuario.findUnique({
+      where: {
+        id_usuario: id,
+      },
+    });
+    return usuario;
+  } catch (error: any) {
+    throw error;
+  }
 }
 
 export async function createUsuario(usuario: UsuarioPostType): Promise<any> {
-    try {
-        const usuarioIgual = await prisma.usuario.findFirst({
-            where: {
-                email: usuario.email
-            }
-        });
+  try {
+    const usuarioIgual = await prisma.usuario.findFirst({
+      where: {
+        email: usuario.email,
+      },
+    });
 
-        if (usuarioIgual) {
-            throw new String("Usuario ja cadatrado!")
-        }
-
-        const hashPassword = await hash(usuario.senha, 8);
-        await prisma.usuario.create({
-            data: {
-                nome: usuario.nome,
-                admin: usuario.admin,
-                email: usuario.email,
-                id_usuario: usuario.id_usuario,
-                senha: hashPassword
-            }
-        });
-
-        return {
-            msg: `Usuario ${usuario.id_usuario} criado com sucesso!`
-        }
-    } catch(error: any) {
-        throw error;
+    if (usuarioIgual) {
+      throw new String("Usuario ja cadatrado!");
     }
+
+    const hashPassword = await hash(usuario.senha, 8);
+    await prisma.usuario.create({
+      data: {
+        nome: usuario.nome,
+        admin: usuario.admin,
+        email: usuario.email,
+        id_usuario: usuario.id_usuario,
+        senha: hashPassword,
+      },
+    });
+
+    return {
+      msg: `Usuario ${usuario.id_usuario} criado com sucesso!`,
+    };
+  } catch (error: any) {
+    throw error;
+  }
 }
 
 export async function alterUsuario(id: number, usuario: UsuarioPutType) {
-    try {
-        await prisma.usuario.update({
-            where: {
-                id_usuario: id
-            },
-            data: usuario
-        });
+  try {
+    await prisma.usuario.update({
+      where: {
+        id_usuario: id,
+      },
+      data: usuario,
+    });
 
-        return await prisma.usuario.findUnique({
-            where: {
-                id_usuario: id
-            }
-        });
-
-    } catch(error: any) {
-        throw error;
-    }
+    return await prisma.usuario.findUnique({
+      where: {
+        id_usuario: id,
+      },
+    });
+  } catch (error: any) {
+    throw error;
+  }
 }
 
 export async function removeUsuario(id: number) {
-    try {
-        await prisma.usuario.delete({
-            where: {
-                id_usuario: id
-            }
-        });
-        return {
-            msg: `Usuario ${id} deletado com sucesso!!`
-        }
-    } catch(error: any) {
-        throw error;
-    }
+  try {
+    await prisma.usuario.delete({
+      where: {
+        id_usuario: id,
+      },
+    });
+    return {
+      msg: `Usuario ${id} deletado com sucesso!!`,
+    };
+  } catch (error: any) {
+    throw error;
+  }
 }
